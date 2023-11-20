cd1312b1d63ab903a728e4d5c85e1a3b5cf6839b
anothernickguy <132588165+anothernickguy@users.noreply.github.com>
2023-11-20 17:12:59 -0300

    merge

--- Assets/C#/EnemyFollow.cs
+++ Assets/C#/EnemyFollow.cs
@@ -10,7 +10,7 @@ public class EnemyFollow : MonoBehaviour
 {
     public Transform player; // Referencia al transform del jugador
     public float velocidad = 5f; // Velocidad de persecución
-    public NavMeshAgent myAgent;
+    [SerializeField] NavMeshAgent myAgent;
     void Update()
     {
         if (player == null)
@@ -21,15 +21,8 @@ public class EnemyFollow : MonoBehaviour
 
         // Calcula la dirección hacia el jugador
         Vector3 direccion = player.position - transform.position;
-        //direccion.y = 0; // Mantén el enemigo a nivel del suelo
+
         myAgent.SetDestination(player.position);
-
-        // Normaliza la dirección para que el enemigo no se mueva más rápido en diagonales
-        // direccion.Normalize();
-
-        // Mueve al enemigo en la dirección del jugador
-        // transform.Translate(direccion * velocidad * Time.deltaTime);
-
 
         // Opcional: mira hacia la dirección del jugador (descomenta la siguiente línea si deseas activar esto)
         // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direccion), 0.1f);
